a0ab5b463078fefc5a5f420b7a1d43dfd065cb0e
Ömer Erenler <65962653+omerfarukerenler@users.noreply.github.com>
2021-12-09 13:49:08 +0300

    Delete odev06 directory

--- odev06/src/server/server.js
+++
@@ -1,9 +0,0 @@
-const app =require('./app');
-const server = require('http').Server(app);
-
-const port = process.env.PORT || 8080 ;
-app.listen(port, () =>{
-    console.log("Server başladı " + port);
-})
-
-
